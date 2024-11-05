41a4901d2c1a0da4071e40420c40124ac980a1a1
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-11-05 08:42:03 +0000

    Create bolerplate for products pages, including styling

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def products(request):
+    """ A view to render products page """
+    return render(request, 'products/products.html')
