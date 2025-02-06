52d710011f72c7d95d7864ed79088ea96f309913
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-06 03:02:40 +0000

    Make changes to fixture. Make change to CatGroup model. Modify code to comply with PEP 8

--- products/models.py
+++ products/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class CatGroup(models.Model):
     group_name = models.CharField(max_length=30)
-    group_vat = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    group_vat = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True, default=0
+    )
     group_pic_loc = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -16,7 +19,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     category_name = models.CharField(max_length=30)
-    group_id = models.ForeignKey('CatGroup', null=True, blank=True, on_delete=models.SET_NULL) 
+    group_id = models.ForeignKey(
+        'CatGroup', null=True, blank=True, on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return self.category_name
@@ -30,8 +35,12 @@ class Manufacturer(models.Model):
 
 
 class Product(models.Model):
-    category_id = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) 
-    manufacturer_id = models.ForeignKey('Manufacturer', null=True, blank=True, on_delete=models.SET_NULL) 
+    category_id = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    manufacturer_id = models.ForeignKey(
+        'Manufacturer', null=True, blank=True, on_delete=models.SET_NULL
+    )
     product_name = models.CharField(max_length=32)
     description = models.TextField(max_length=1000, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
