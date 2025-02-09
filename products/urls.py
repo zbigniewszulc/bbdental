d411bb0967f59b2d2794f3a2640b3446c3e58379
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-09 21:03:45 +0000

    Display products filtered by subcategory

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('all/', views.all_products, name='all_products'),
-    path('menu/', views.products_menu, name='products_menu'),
     path('<int:category_id>/', views.products_by_category,
-         name='products_by_category')
+         name='products_by_category'),
+    path('<int:category_id>/<int:subcategory_id>/',
+         views.products_by_subcategory, name='products_by_subcategory'),
+    path('menu/', views.products_menu, name='products_menu')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Product, Category, Subcategory
 
 # Create your views here.
 
@@ -30,7 +30,25 @@ def all_products(request):
 
 
 def products_by_category(request, category_id):
-    """ A view to display products filtered by category """
+    """
+    A view to display products filtered by category.
+    Display all :model:`products.Product` that are
+    filtered by :model:`products.Category`.
+
+    **Context**
+
+    ``category``
+        An instance of :model:`products.Category`.
+    ``products``
+        A queryset of :model:`products.Product` filtered by category.
+    ``categories``
+        A queryset of all :model:`products.Category`, used for menu display.
+
+    **Template**
+
+    :template:`products/products.html`.
+    """
+
     category = get_object_or_404(Category, id=category_id)
     products = Product.objects.filter(
         subcategory_id__category_id=category.id).order_by('product_name')
@@ -38,6 +56,47 @@ def products_by_category(request, category_id):
 
     context = {
         'category': category,
+        'products': products,
+        'categories': categories
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def products_by_subcategory(request, category_id, subcategory_id):
+    """
+    A view to display products filtered by subcategory.
+    Display all :model:`products.Product` that are
+    filtered by :model:`products.Subcategory` and :model:`products.Category`.
+
+    **Context**
+
+    ``category``
+        An instance of :model:`products.Category`.
+    ``subcategory``
+        An instance of :model:`products.Subcategory`.
+    ``products``
+        A queryset of :model:`products.Product` filtered by subcategory.
+    ``categories``
+        A queryset of all :model:`products.Category`, used for menu display.
+
+    **Template**
+
+    :template:`products/products.html`.
+    """
+
+    category = get_object_or_404(Category, id=category_id)
+    subcategory = get_object_or_404(
+        Subcategory, id=subcategory_id, category_id=category)
+
+    products = Product.objects.filter(
+        subcategory_id=subcategory).order_by('product_name')
+
+    categories = Category.objects.all().order_by('category_name')
+
+    context = {
+        'category': category,
+        'subcategory': subcategory,
         'products': products,
         'categories': categories
     }
