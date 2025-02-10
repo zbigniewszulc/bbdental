76106d61d42278fea9a6dd9af25b65fc4f178f9b
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-10 02:29:10 +0000

    Integrate Bootstrap tooltips

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Initialize Bootstrap tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+});
