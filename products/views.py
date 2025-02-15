f6404f8515bf123f365a34581fdd9cd6d148e9df
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-15 01:34:39 +0000

    Fix next page pagination, add sorting functionality to rest of the views

--- products/views.py
+++ products/views.py
@@ -9,35 +9,49 @@ from .models import Product, Category, Subcategory
 
 def all_products(request):
     """
-    A view to render all roducts page.
+    A view to render all products page.
     Display all :model:`products.Product`.
 
     **Context**
 
-    ``products``
-        An instance of :model:`products.Product`.
+    ``categories``
+        A queryset of all :model:`products.Category`, used for menu display.
+    ``page_obj``
+        A paginated queryset of :model:`products.Product`.
+    ``sort``
+        Selected sorting criteria.
+    ``direction``
+        Selected sorting direction.
 
     **Template**
 
     :template:`products/products.html`.
     """
-    
-    # performance issue
+
+    # select_related and prefetch_related used to solve database query
+    # performance issues
+    products = (
+        Product.objects
+        .annotate(
+            lower_product_name=Lower('product_name'),
+            lower_manufacturer_name=Lower('manufacturer_id__manufacturer_name')
+        )
+        .select_related('subcategory_id', 'manufacturer_id')
+    )
+    # Fetch categories
+    categories = Category.objects.prefetch_related('subcategories').annotate(
+        lower_category_name=Lower('category_name')).order_by(
+            'lower_category_name')
 
     # Get sorting parameters with default values
     sort = request.GET.get('sort', 'name')
     direction = request.GET.get('direction', 'asc')
 
-    # select_related and prefetch_related used to solve databse query
-    # performance
-    products = Product.objects.annotate(lower_product_name=Lower(
-        'product_name')).select_related('subcategory_id', 'manufacturer_id')
-                 
     # Sorting
     if sort == 'name':
         sortkey = 'lower_product_name'
     elif sort == 'manufacturer':
-        sortkey = 'manufacturer_id'
+        sortkey = 'lower_manufacturer_name'
     elif sort == 'price':
         sortkey = 'price'
     else:
@@ -47,11 +61,6 @@ def all_products(request):
         sortkey = f'-{sortkey}'
 
     products = products.order_by(sortkey)
-
-    # Fetch categories
-    categories = Category.objects.prefetch_related('subcategories').annotate(
-        lower_category_name=Lower('category_name')).order_by(
-            'lower_category_name')
 
     # Pagination: 20 products per page
     paginator = Paginator(products, 20)
@@ -75,33 +84,68 @@ def products_by_category(request, category_id):
 
     **Context**
 
-    ``category``
+    ```category``
         An instance of :model:`products.Category`.
-    ``products``
-        A queryset of :model:`products.Product` filtered by category.
     ``categories``
         A queryset of all :model:`products.Category`, used for menu display.
-
+    ``page_obj``
+        A paginated queryset of :model:`products.Product` filtered by category.
+    ``sort``
+        Selected sorting criteria.
+    ``direction``
+        Selected sorting direction.
     **Template**
 
     :template:`products/products.html`.
     """
     # select_related used to solve databse query performance issue
     category = get_object_or_404(Category, id=category_id)
-    products = Product.objects.filter(
-        subcategory_id__category_id=category.id).select_related(
-            'subcategory_id', 'manufacturer_id').order_by('product_name')
-    categories = Category.objects.all().order_by('category_name')
+    products = (
+        Product.objects
+        .filter(subcategory_id__category_id=category.id)
+        .annotate(
+            lower_product_name=Lower('product_name'), 
+            lower_manufacturer_name=Lower('manufacturer_id__manufacturer_name')
+        )
+        .select_related('subcategory_id', 'manufacturer_id')
+    )
+    categories = (
+        Category.objects
+        .prefetch_related('subcategories')
+        .annotate(lower_category_name=Lower('category_name'))
+        .order_by('lower_category_name')
+    )
+
+    # Get sorting parameters with default values
+    sort = request.GET.get('sort', 'name')
+    direction = request.GET.get('direction', 'asc')
+
+    # Sorting
+    if sort == 'name':
+        sortkey = 'lower_product_name'
+    elif sort == 'manufacturer':
+        sortkey = 'manufacturer_id'
+    elif sort == 'price':
+        sortkey = 'price'
+    else:
+        sortkey = 'lower_product_name'  # Default sorting
+
+    if direction == 'desc':
+        sortkey = f'-{sortkey}'
+
+    products = products.order_by(sortkey)
 
     # Pagination: 20 products per page
-    paginator = Paginator(products, 20)  
+    paginator = Paginator(products, 20)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
         'category': category,
         'categories': categories,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'sort': sort,
+        'direction': direction
     }
 
     return render(request, 'products/products.html', context)
@@ -119,10 +163,14 @@ def products_by_subcategory(request, category_id, subcategory_id):
         An instance of :model:`products.Category`.
     ``subcategory``
         An instance of :model:`products.Subcategory`.
-    ``products``
-        A queryset of :model:`products.Product` filtered by subcategory.
     ``categories``
         A queryset of all :model:`products.Category`, used for menu display.
+    ``page_obj``
+        Paginated queryset of :model:`products.Product` filtered by subcategory
+    ``sort``
+        Selected sorting criteria.
+    ``direction``
+        Selected sorting direction.
 
     **Template**
 
@@ -132,10 +180,37 @@ def products_by_subcategory(request, category_id, subcategory_id):
     category = get_object_or_404(Category, id=category_id)
     subcategory = get_object_or_404(
         Subcategory, id=subcategory_id, category_id=category)
-    products = Product.objects.filter(
-        subcategory_id=subcategory).select_related(
-        'subcategory_id', 'manufacturer_id').order_by('product_name')
-    categories = Category.objects.all().order_by('category_name')
+    products = (
+        Product.objects
+        .filter(subcategory_id=subcategory)
+        .annotate(
+            lower_product_name=Lower('product_name'),
+            lower_manufacturer_name=Lower('manufacturer_id__manufacturer_name')
+        )
+        .select_related('subcategory_id', 'manufacturer_id')
+    )
+    categories = Category.objects.prefetch_related('subcategories').annotate(
+        lower_category_name=Lower('category_name')).order_by(
+            'lower_category_name')
+
+    # Get sorting parameters with default values
+    sort = request.GET.get('sort', 'name')
+    direction = request.GET.get('direction', 'asc')
+
+    # Sorting
+    if sort == 'name':
+        sortkey = 'lower_product_name'
+    elif sort == 'manufacturer':
+        sortkey = 'manufacturer_id'
+    elif sort == 'price':
+        sortkey = 'price'
+    else:
+        sortkey = 'lower_product_name'  # Default sorting
+
+    if direction == 'desc':
+        sortkey = f'-{sortkey}'
+
+    products = products.order_by(sortkey)
 
     # Pagination: 20 products per page
     paginator = Paginator(products, 20)  
@@ -146,7 +221,9 @@ def products_by_subcategory(request, category_id, subcategory_id):
         'category': category,
         'subcategory': subcategory,
         'categories': categories,
-        'page_obj': page_obj
+        'page_obj': page_obj,      
+        'sort': sort,
+        'direction': direction
     }
 
     return render(request, 'products/products.html', context)
