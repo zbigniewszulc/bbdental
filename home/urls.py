eb60fe560ae0f402ab2c76faaf2b06797bcc6e07
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-17 06:10:46 +0000

    Add privacy policy and T&S pages

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service')
 ]
--- home/views.py
+++ home/views.py
@@ -13,3 +13,66 @@ def index(request):
     """
 
     return render(request, 'home/index.html')
+
+
+def privacy_policy(request):
+    """
+    A view to render the Privacy Policy page with pdf's
+
+    **Context**
+
+    ``documents``
+        A list of dictionaries containing:
+        - ``name``: Title of the privacy-related document.
+        - ``file``: Filename of the document stored in the static directory
+
+    **Template**
+    :template:`legal/privacy_policy.html`.
+    """
+    privacy_pdfs = [
+        {
+            "name": "Privacy and Cookies Policy", 
+            "file": "privacy-and-cookies-policy.pdf"
+        },
+        {
+            "name": "Processing of Personal Data", 
+            "file": "processing-of-personal-data.pdf"
+        }
+    ]
+
+    context = {
+        'documents': privacy_pdfs,
+    }
+
+    return render(request, 'legal/privacy_policy.html', context)
+
+
+def terms_of_service(request):
+    """
+    A view to render the T&S page with pdf's
+
+    **Context**
+
+    ``documents``
+        A list of dictionaries containing:
+        - ``name``: Title of the Terms of Service document.
+        - ``file``: Filename of the document stored in the static directory.
+
+    **Template**
+    :template:`legal/terms_of_service.html`.
+    """
+    terms_pdfs = [
+        {
+            "name": "Online Store Regulations", 
+            "file": "online-storage-regulations.pdf"},
+        {
+            "name": "Right to Withdraw", 
+            "file": "right-to-withdraw.pdf"
+        }
+    ]
+
+    context = {
+         'documents': terms_pdfs,
+    }
+
+    return render(request, 'legal/terms_of_service.html', context)
