14db3c8fc602293482b0744e851d9c419cc9493d
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-02-21 00:39:14 +0000

    Add tosts, replace previous messages area and add relevant messages on bag content updates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -17,33 +19,63 @@ def view_bag(request):
 
 def add_to_bag(request, product_id):
     """
-    Add particular product to the bag
+    Add particular product to the shopping bag
     """
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    try:
+        product = get_object_or_404(Product, pk=product_id)
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
 
-    if product_id in list(bag.keys()):
-        bag[product_id] += quantity
-    else:
-        bag[product_id] = quantity
+        if product_id in list(bag.keys()):
+            bag[product_id] += quantity
+            messages.success(
+                request,
+                f'{product.product_name} has been added to the shopping bag'
+            )
+        else:
+            bag[product_id] = quantity
+            messages.success(
+                request,
+                f'{product.product_name} has been added to the shopping bag'
+            )
 
-    request.session['bag'] = bag
+        request.session['bag'] = bag
+
+    except Exception as e:
+        messages.error(request, f'Error occurred: {e}')
+
     return redirect(redirect_url)
 
 
 def update_bag(request):
+    """
+    Update quantity of product in the shopping bag or remove it.
+    """
     if request.method == "POST":
-        product_id = request.POST.get("product_id")
-        quantity = int(request.POST.get("quantity", 1))
+        try:
+            product_id = request.POST.get("product_id")
+            quantity = int(request.POST.get("quantity", 1))
+            product = get_object_or_404(Product, pk=product_id)
 
-        bag = request.session.get("bag", {})
+            bag = request.session.get("bag", {})
 
-        if quantity > 0:
-            bag[product_id] = quantity  # Update quantity
-        else:
-            bag.pop(product_id, None)  # Remove item if quantity is 0
+            if quantity > 0:
+                bag[product_id] = quantity  # Update quantity
+                messages.success(
+                    request,
+                    f'Quantity of the {product.product_name} has been updated'
+                )
+            else:
+                bag.pop(product_id, None)  # Remove item if quantity is 0
+                messages.warning(
+                    request,
+                    f'{product.product_name} has been removed '
+                    'from the shopping bag'
+                )
+            request.session["bag"] = bag  # Save updated bag to session
 
-        request.session["bag"] = bag  # Save updated bag to session
+        except Exception as e:
+            messages.error(request, f'Error occurred: {e}')
 
     return redirect("view_bag")
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Initialize Bootstrap tooltips
+    // Initialise Bootstrap tooltips
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
