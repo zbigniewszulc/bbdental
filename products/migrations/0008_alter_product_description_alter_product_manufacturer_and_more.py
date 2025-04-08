677d73264f199d808cfcaafa213e347ae6e8473c
Zbigniew Szulc <zbigniewszulc@gmail.com>
2025-04-08 04:36:26 +0100

    Amend Products app migration file by removing default as it was previosuly created to fix database issue

--- products/migrations/0008_alter_product_description_alter_product_manufacturer_and_more.py
+++ products/migrations/0008_alter_product_description_alter_product_manufacturer_and_more.py
@@ -14,25 +14,25 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='product',
             name='description',
-            field=models.TextField(default='No describtion', max_length=1000),
+            field=models.TextField(max_length=1000),
             preserve_default=False,
         ),
         migrations.AlterField(
             model_name='product',
             name='manufacturer',
-            field=models.ForeignKey(default=52, on_delete=django.db.models.deletion.PROTECT, related_name='manufacturer_products', to='products.manufacturer'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='manufacturer_products', to='products.manufacturer'),
             preserve_default=False,
         ),
         migrations.AlterField(
             model_name='product',
             name='subcategory',
-            field=models.ForeignKey(default=23, on_delete=django.db.models.deletion.PROTECT, related_name='subcategory_products', to='products.subcategory'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='subcategory_products', to='products.subcategory'),
             preserve_default=False,
         ),
         migrations.AlterField(
             model_name='subcategory',
             name='category',
-            field=models.ForeignKey(default=8, on_delete=django.db.models.deletion.PROTECT, related_name='subcategories', to='products.category'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='subcategories', to='products.category'),
             preserve_default=False,
         ),
     ]
