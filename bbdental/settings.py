ca38b0956c40189468d4302a9b4d9357cd888d00
Zbigniew Szulc <zbigniewszulc@gmail.com>
2026-08-22 22:04:59 +0100

    Remove unused and old Cloudinary storage package (j3-cloudinary-storage)

--- bbdental/settings.py
+++ bbdental/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
